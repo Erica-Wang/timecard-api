202032532cfa00f480951972ad712226561686fc
Erica Wang <ericawang@Ericas-MacBook-Pro.local>
2020-04-20 22:55:38 -0400

    fix

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const dotenv = require('dotenv');
 const routes = require('./routes');
 require('dotenv').config();
 
-express()
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.json({}))
-  .listen(5000, () => console.log(`Listening on 5000 }`))
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', routes);
+
+
+const server = app.listen(5000, function(){
+ console.log('Listening on port 5000');
+});
